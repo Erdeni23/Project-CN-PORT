6e619e67b3c102e446f5c7fd3127120bf376b3e9
Erdeni Arsalanov <erdeni2001@yandex.ru>
2025-07-04 11:03:05 +0800

    Adding WeaponsInterface
    Communicating between weapons, weapons owners and projectiles

---
+++ Source/CPPandBP_Portfolio/Private/Interfaces/WeaponsInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/WeaponsInterface.h"
+
+// Add default functionality here for any IWeaponsInterface functions that are not pure virtual.
